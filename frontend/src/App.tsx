4fe77559734021120975ced77d308ea65c25c55a
Stabbershade <Stabbershade@hotmail.com>
2023-09-06 17:51:05 +0800

    <feat>(UI): Connecting frontend to backend in Application_Main, src ,static and templates folder

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import { BrowserRouter as Router, Routes , Route , Link , redirect} from "react-router-dom"
+
+export const App = () =>{
+  return (
+    <div>
+        <h1>This is a Test</h1>
+    </div>
+  );
+}
