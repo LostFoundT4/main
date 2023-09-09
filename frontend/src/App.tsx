10fa3ff777c2bd73ea7c0470f52c1010800b2537
Teffy <tcanabel.2022@scis.smu.edu.sg>
2023-09-09 23:51:42 +0800

    Added Rough Dashboard Template
    Used a template from mui and included all the necessary dependencies, enclosed in a folder called profile_dependencies.
    
    Note that Charts.tsx requires an additional dependency called "recharts". This can be downloaded by writing "npm install recharts" in the terminal within the frontend folder.
    
    The SignUp and SignIn Pages are done by Matthew, and are subject to changes from his side.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,26 @@
-import { BrowserRouter as Router, Routes , Route , Link , redirect} from "react-router-dom"
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+    redirect,
+} from "react-router-dom";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import LandingPage from "./components/LandingPage";
+import SignInSide from "./components/SignInSide";
+import SignUpSide from "./components/SignUpSide";
+import ProfilePage from "./components/ProfilePage";
 
-export const App = () =>{
-  return (
-    <div>
-        <h1>This is a Test</h1>
-    </div>
-  );
-}
+export const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="frontend/" element={<LandingPage />}></Route>
+                <Route path="frontend/sign-in" element={<SignIn />}></Route>
+                <Route path="frontend/sign-up" element={<SignUp />}></Route>
+                <Route path="frontend/profile-page" element={<ProfilePage />}></Route>
+            </Routes>
+        </Router>
+    );
+};
---
+++ frontend/src/components/SignUpSide.tsx
@@ -0,0 +1,180 @@
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Stack from "@mui/material/Stack";
+import Container from "@mui/material/Container";
+
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+
+export default function SignUpSide() {
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get("email"),
+            password: data.get("password"),
+        });
+    };
+
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <Grid container component="main" sx={{ height: "100vh" }}>
+                <CssBaseline />
+                <Grid
+                    item
+                    xs={12}
+                    sm={8}
+                    md={7} // Increase the md value to allocate more space
+                    sx={{
+                        bgcolor: "background.paper",
+                        pt: 8,
+                        pb: 6,
+                    }}
+                >
+                    <Container maxWidth="sm">
+                        <Typography
+                            component="h1"
+                            variant="h3"
+                            align="center"
+                            color="text.primary"
+                            gutterBottom
+                        >
+                            Welcome to <br /> SMU Lost and Found
+                        </Typography>
+                        <Typography
+                            variant="h5"
+                            align="center"
+                            color="text.secondary"
+                            paragraph
+                        >
+                            Lost something? Find it here! <br />
+                            Found something? Upload it here!
+                        </Typography>
+                        <Stack
+                            sx={{ pt: 1 }}
+                            direction="row"
+                            spacing={2}
+                            justifyContent="center"
+                        ></Stack>
+                    </Container>
+                </Grid>{" "}
+                <Grid
+                    item
+                    xs={12}
+                    sm={8}
+                    md={5}
+                    component={Paper}
+                    elevation={6}
+                    square
+                >
+                    <Box
+                        sx={{
+                            my: 2,
+                            mx: 4,
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Sign up
+                        </Typography>
+                        <Box
+                            component="form"
+                            noValidate
+                            onSubmit={handleSubmit}
+                            sx={{ mt: 3 }}
+                        >
+                            {" "}
+                            <Grid container spacing={2}>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        name="school-email"
+                                        label="School Email"
+                                        type="school-email"
+                                        id="school-email"
+                                        autoComplete="school-email"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        name="password"
+                                        label="Password"
+                                        type="password"
+                                        id="password"
+                                        autoComplete="new-password"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        name="username"
+                                        label="Username"
+                                        type="username"
+                                        id="username"
+                                        autoComplete="username"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        fullWidth
+                                        name="phone-number"
+                                        label="Phone Number (Optional)"
+                                        type="phone-number"
+                                        id="phone-number"
+                                        autoComplete="phone-number"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        fullWidth
+                                        name="telegram-handle"
+                                        label="Telegram Handle (Optional)"
+                                        type="telegram-handle"
+                                        id="telegram-handle"
+                                        autoComplete="telegram-handle"
+                                    />
+                                </Grid>
+                            </Grid>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                Sign Up
+                            </Button>
+                            <Grid container justifyContent="flex-end">
+                                <Grid item>
+                                    <Link href="sign-in" variant="body2">
+                                        Already have an account? Sign in
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+        </ThemeProvider>
+    );
+}
