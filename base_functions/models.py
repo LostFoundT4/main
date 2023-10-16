9405f8b589b37464eb6f31325129bd9b28583cb6
jayoon9916 <cheesen.oon.2022@scis.smu.edu.sg>
2023-10-16 21:44:23 +0800

    <fix> Changes to 'on_delete' condition for all tables

--- base_functions/models.py
+++ base_functions/models.py
@@ -13,7 +13,7 @@ class Ticket (models.Model):
 
 class Item (models.Model):
     itemID = models.AutoField(primary_key=True)
-    ticketID = models.ForeignKey(Ticket, related_name='items', verbose_name="ticketID", on_delete=models.SET_DEFAULT, default=None)
+    ticketID = models.ForeignKey(Ticket, related_name='items', verbose_name="ticketID", on_delete=models.CASCADE, default=None)
     itemName = models.CharField(max_length=200)
     category = models.CharField(max_length=200)
     image = models.ImageField(upload_to='images/', blank=True, null=True)
@@ -21,19 +21,19 @@ class Item (models.Model):
 
 class UserAdditionalProfile(models.Model):
     userProfileNumber = models.AutoField(primary_key = True)
-    user = models.ForeignKey(User, related_name='profile', on_delete=models.SET_DEFAULT, default=None)
+    user = models.ForeignKey(User, related_name='profile', on_delete=models.CASCADE, default=None)
     userTelegramID = models.CharField(max_length=100, blank=True, null=True)
     userProfilePicture = models.ImageField(upload_to='profile-images/', blank=True, null=True)
     userPhoneNumber = models.CharField(max_length=8, blank=True)
 
 class Reputation(models.Model):
     reputationID = models.AutoField(primary_key = True)
-    user = models.ForeignKey(User, related_name='reputation', on_delete=models.SET_DEFAULT, default=None)
+    user = models.ForeignKey(User, related_name='reputation', on_delete=models.CASCADE, default=None)
     flagged = models.IntegerField(blank=True, null=True)
     score = models.IntegerField(blank=True, null=True)
 
 class Blacklist(models.Model):
     blacklistID = models.AutoField(primary_key= True)
-    user = models.ForeignKey(User, related_name='blacklist', on_delete=models.SET_DEFAULT, default=None)    
-    ticket = models.ForeignKey(Ticket, related_name='blacklist', on_delete=models.SET_DEFAULT, default=None)
+    user = models.ForeignKey(User, related_name='blacklist', on_delete=models.CASCADE, default=None)    
+    ticket = models.ForeignKey(Ticket, related_name='blacklist', on_delete=models.CASCADE, default=None)
     timestamp = models.DateTimeField(auto_now_add=True)
