20caa1db0d7a9f6e96d7571694b094289f0efe49
Matthew <matthew.goh.2022@scis.smu.edu.sg>
2023-10-20 17:20:17 +0800

    <refactor> Consolidated import statements and added comments

--- frontend/src/components/protectedRoute/protectedRoute.tsx
+++ frontend/src/components/protectedRoute/protectedRoute.tsx
@@ -1,12 +1,14 @@
-import { Outlet , Navigate} from "react-router-dom";
-import {useState , useEffect} from "react";
+import { Outlet, Navigate} from "react-router-dom";
 import {useContext} from "react";
-import {UserIDContext , UserNameContext} from "../../utils/contextConfig"
+import {UserIDContext, UserNameContext} from "../../utils/contextConfig"
 import AxiosInstance from "../../utils/axiosInstance";
 
+// Custom hook to check user authentication and set context values
 const useAuth  = () =>{
-    const {contextID, setContextID} = useContext(UserIDContext)
-    const {contextName, setContextName} = useContext(UserNameContext)
+    const {contextID, setContextID} = useContext(UserIDContext);
+    const {contextName, setContextName} = useContext(UserNameContext);
+
+    // Check if authToken is present in localStorage
     if(localStorage.getItem('authToken') === undefined || localStorage.getItem('authToken') === null){
         return false;
     }
