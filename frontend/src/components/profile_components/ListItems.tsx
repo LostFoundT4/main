7374f56edc3f4e31a77aa3a6eeb1f9bc6624b34d
Matthew <matthew.goh.2022@scis.smu.edu.sg>
2023-10-20 17:14:55 +0800

    <refactor> Consolidated import statements

--- frontend/src/components/profile_components/ListItems.tsx
+++ frontend/src/components/profile_components/ListItems.tsx
@@ -1,55 +1,43 @@
-import * as React from "react";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import PeopleIcon from "@mui/icons-material/People";
-import SettingsIcon from "@mui/icons-material/Settings";
-import LogoutIcon from "@mui/icons-material/Logout";
-import HomeIcon from '@mui/icons-material/Home';
-import AxiosInstance from "../../utils/axiosInstance";
-import { useNavigate } from "react-router-dom";
-
-export const mainListItems = (
-    <React.Fragment>
-        <ListItemButton href="/frontend/home">
-            <ListItemIcon>
-                <HomeIcon />
-            </ListItemIcon>
-            <ListItemText primary="Home" />
-        </ListItemButton>
-        <ListItemButton href="/frontend/my-listings">
-            <ListItemIcon>
-                <ShoppingCartIcon />
-            </ListItemIcon>
-            <ListItemText primary="My Listings" />
-        </ListItemButton>
-    </React.Fragment>
-);
-
+import { Fragment } from "react";
+import { ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import { ShoppingCart, Settings, Logout, Home } from "@mui/icons-material";
 
 const handleLogout = () => {
     localStorage.removeItem('authToken')
 }
 
+// Top half icons of sidebar
+export const mainListItems = (
+    <Fragment>
+        <ListItemButton href="/frontend/home">
+            <ListItemIcon>
+                <Home />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+        </ListItemButton>
+        <ListItemButton href="/frontend/my-listings">
+            <ListItemIcon>
+                <ShoppingCart />
+            </ListItemIcon>
+            <ListItemText primary="My Listings" />
+        </ListItemButton>
+    </Fragment>
+);
 
+// Bottom half icons of sidebar
 export const secondaryListItems = (
-    <React.Fragment>
-        {/* <ListSubheader component="div" inset>
-      Others
-    </ListSubheader> */}
+    <Fragment>
         <ListItemButton href="/frontend/edit-profile">
             <ListItemIcon>
-                <SettingsIcon />
+                <Settings />
             </ListItemIcon>
             <ListItemText primary="Edit Profile" />
         </ListItemButton>
         <ListItemButton onClick={handleLogout} href="/frontend/sign-in">
             <ListItemIcon>
-                <LogoutIcon />
+                <Logout />
             </ListItemIcon>
             <ListItemText primary="Sign Out" />
         </ListItemButton>
-    </React.Fragment>
+    </Fragment>
 );
