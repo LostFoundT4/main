ab69eed36bfada9da86769c7a94d3d490578c59c
jayoon9916 <cheesen.oon.2022@scis.smu.edu.sg>
2023-10-24 21:13:32 +0800

    <feat> Update reporting

--- reporting/models.py
+++ reporting/models.py
@@ -3,21 +3,29 @@ from base_functions.models import Ticket, Item
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Status(models.Model):
+    statusID = models.AutoField(primary_key=True)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ticket = models.ForeignKey(Ticket,on_delete=models.CASCADE)
+    status = models.CharField(max_length=200)
+    endorsedUserID = models.IntegerField(blank=True, null=True)
+    counter = models.IntegerField(blank=True, null=True)
+    previous_counter = models.IntegerField(default=0)
+
 class ReportInfo(models.Model):
     reportInfoID = models.AutoField(primary_key=True)
     ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     location = models.ForeignKey(to='Location', on_delete=models.CASCADE)
     description = models.CharField(max_length=300)
-
-class Status(models.Model):
-    statusID = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    ticket = models.ForeignKey(Ticket,on_delete=models.CASCADE)
-    type = models.CharField(max_length=200)
-    endorsedUserID = models.IntegerField(blank=True, null=True)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE )
 
 class Location (models.Model):
     locationID = models.AutoField(primary_key=True)
     building = models.CharField(max_length=200)
     room = models.CharField(max_length=200)
+
+class PendingUsers(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.ForeignKey(Status,related_name="pendingUsers", on_delete=models.CASCADE)
--- reporting/serializers.py
+++ reporting/serializers.py
@@ -1,38 +1,52 @@
+from asyncore import read
 from rest_framework import serializers
-from .models import ReportInfo, Status, Location
-from base_functions.serializers import TicketSerializer, ItemSerializer
+from .models import ReportInfo, Status, Location, PendingUsers
+from base_functions.serializers import TicketSerializer, ItemSerializer, CurrentUserSerializer
+from django.contrib.auth.models import User
 
 class LocationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Location
         fields = '__all__'
 
+class PendingUsersSerializer(serializers.ModelSerializer):
+    # user = CurrentUserSerializer()
+    username = serializers.CharField(source="user.username")
+    
+    class Meta:
+        model = PendingUsers
+        fields = ['id', 'user', 'username', 'status']
+        
+class StatusSerializer(serializers.ModelSerializer):
+    ticket = TicketSerializer()
+    pendingUsers = PendingUsersSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Status
+        fields = "__all__"
+
 class ReportSerializer(serializers.ModelSerializer):
     ticket = TicketSerializer()
     location = LocationSerializer()
     item = ItemSerializer()
+    status = StatusSerializer()
 
     class Meta:
         model = ReportInfo
-        fields = ['reportInfoID', 'description', 'ticket', 'location', 'item']
-
-
-class StatusSerializer(serializers.ModelSerializer):
-    ticket = TicketSerializer()
-
-    class Meta:
-        model = Status
-        fields = ['statusID','user_id', 'ticket', 'type', 'endorsedUserID']
-
+        fields = ['reportInfoID', 'description', 'ticket', 'location', 'item', 'status']
 
 class AlterReportSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = ReportInfo
         fields = "__all__"
 
 class AlterStatusSerializer(serializers.ModelSerializer):
-   
     class Meta:
         model = Status
+        fields = "__all__"
+
+class AlterPendingUsersSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = PendingUsers
         fields = "__all__"
