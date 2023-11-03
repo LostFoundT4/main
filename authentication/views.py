a8eb7a1c7172e1cf51d6e15ee1b0d856df844894
Nyxchaser <tcanabel.2022@scis.smu.edu.sg>
2023-11-03 10:44:52 +0800

    <style> changed email confirmation message to test functionality

--- authentication/views.py
+++ authentication/views.py
@@ -7,9 +7,16 @@ from django.utils.encoding import force_bytes, force_str
 
 from authentication.tokens import account_activation_token
 
-
 # Create your views here.
 def activate(request, uidb64, token):
+    
+    component_content = """
+    <div>
+        <h1>Hello, World!</h1>
+        <p>This is a simple HTML component.</p>
+    </div>
+    """
+    
     try:
         uid = force_str(urlsafe_base64_decode(uidb64))
         user = User.objects.get(pk=uid)
@@ -17,6 +24,7 @@ def activate(request, uidb64, token):
         user = None
     if user is not None and account_activation_token.check_token(user, token):
         login(request, user)
-        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+        #return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+        return HttpResponse(component_content)
     else:
         return HttpResponse('Activation link is invalid!')
