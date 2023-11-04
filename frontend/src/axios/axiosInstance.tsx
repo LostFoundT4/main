b9f4f36edf46796b1bb4eeccbf6146485ed5719f
Oi Jun Jie <junjie.oi.2022@scis.smu.edu.sg>
2023-11-04 21:30:56 +0800

    <feat>Updated AxiosInstance To Reflect Domain Name

--- frontend/src/axios/axiosInstance.tsx
+++ frontend/src/axios/axiosInstance.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-let baseURL = "http://147.182.247.167"; // Towards API-hosted IP address
+let baseURL = "https://www.findmyitem.app"; // Towards API-hosted IP address
 
 export default axios.create({
     baseURL : baseURL
