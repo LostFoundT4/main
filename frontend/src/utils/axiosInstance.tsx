150056fb6eda9e0827333ac9ae9aee0b244b448a
Stabb <Stabbershade@hotmail.com>
2023-11-05 19:58:04 +0800

    <feat> Connect to main site

--- frontend/src/utils/axiosInstance.tsx
+++ frontend/src/utils/axiosInstance.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-let baseURL = "http://127.0.0.1:8080"; // Towards API-hosted IP address
+let baseURL = "https://www.findmyitem.app"; // Towards API-hosted IP address
 
 export default axios.create({
     baseURL : baseURL
